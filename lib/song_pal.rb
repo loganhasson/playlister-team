33312b95dfb491bf6e29192b48863a14e3d4e5e4
Logan Hasson <logan.hasson@gmail.com>
2013-10-13 17:13:38 -0400

    Almost done creating objects

--- lib/song_pal.rb
+++ lib/song_pal.rb
@@ -1,5 +1,7 @@
 require 'pry'
 require_relative 'artist'
+require_relative 'genre'
+require_relative 'song'
 
 class SongPal
 
@@ -25,14 +27,20 @@ class SongPal
     end
   end
 
-  def self.artist_exists?(song_as_array)
+  def self.artist_exists?(song)
     Artist.all.select do |a|
-      a.name == song_as_array[0]
+      a.name == song[0]
     end
   end
 
-  def self.assign_songs_info
-    @@songs_info.each do |song|
+  def self.genre_exists?(song)
+    Genre.all.select do |g|
+      g.name == song[2]
+    end
+  end
+
+  def self.create_artists
+    songs_info.each do |song|
       if artist_exists?(song).size == 0
         artist = Artist.new.tap { |a| a.name = song[0] }
       else
@@ -41,10 +49,44 @@ class SongPal
     end
   end
 
+  def self.create_genres
+    songs_info.each do |song|
+      if genre_exists?(song).size == 0
+        genre = Genre.new.tap { |g| g.name = song[2] }
+      else
+        genre = genre_exists?(song).first
+      end
+    end
+  end
+
+  def self.find_genre(song)
+    Genre.all.each do |g|
+      return g if g.name == song[2]
+    end
+  end
+
+  def self.find_artist(song)
+    Artist.all.each do |a|
+      return a if a.name == song[0]
+    end
+  end
+
+  def self.create_songs
+    songs_info.each do |song|
+      new_song = Song.new.tap do |s|
+        s.name = song[1]
+        s.genre = find_genre(song)
+      end
+      find_artist(song).add_song(new_song)
+    end
+  end
 
 end
 
 SongPal.split_song_info
-SongPal.assign_songs_info
+SongPal.create_artists
+SongPal.create_genres
+SongPal.create_songs
+binding.pry
 
 # For every file, make the artist, then the genre, then the song?
