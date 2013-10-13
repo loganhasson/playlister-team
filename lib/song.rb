ba02594837918f314df84f3edbf4c080793484c3
Logan Hasson <logan.hasson@gmail.com>
2013-10-13 15:54:55 -0400

    Add regex to split songs

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,16 @@
 class Song
 
-  attr_accessor :genre, :name, :artist
+  attr_accessor :name
+  attr_reader :genre, :artist
 
   def genre=(genre)
     @genre = genre
     genre.songs << self
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
   end
 
 end
---
+++ lib/song_pal.rb
@@ -0,0 +1,37 @@
+require 'pry'
+class SongPal
+
+  @@songs_info = []
+
+  def self.get_song_info
+    Dir.entries('data/.').map do |f|
+      f.gsub('.mp3', '') if !f.start_with?('.')
+    end.compact
+  end
+
+  def self.split_song_info
+    get_song_info.each do |track|
+      split = track.split(' - ')
+      artist = split[0]
+      song = split[1].split(' [')[0]
+      binding.pry
+      genre = split[1].match(/(?<=\[)\w*(?=\])/,'')
+      binding.pry
+    end
+  end
+
+    # hash = {"Artist" =>
+    #           :genres =>
+    #             :folk => [songs]
+    #             :rap => [songs]
+    #        }
+
+end
+
+SongPal.split_song_info
+
+# Get all the file names from data directory
+
+# Split them up into artist, song name, and genre (hash?)
+
+# For every file, make the artist, then the genre, then the song?
