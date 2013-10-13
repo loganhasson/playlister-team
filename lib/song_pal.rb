2582cc002b806795e65a1287848d5dc8c8e0ce22
Logan Hasson <logan.hasson@gmail.com>
2013-10-13 16:40:21 -0400

    Create Artist objects

--- lib/song_pal.rb
+++ lib/song_pal.rb
@@ -1,7 +1,13 @@
 require 'pry'
+require_relative 'artist'
+
 class SongPal
 
   @@songs_info = []
+
+  def self.songs_info
+    @@songs_info
+  end
 
   def self.get_song_info
     Dir.entries('data/.').map do |f|
@@ -14,23 +20,31 @@ class SongPal
       split = track.split(' - ')
       artist = split[0]
       song = split[1].split(' [')[0]
-      genre = split[1].match(/(?<=\[)\w*(?=\])/).to_s
-      binding.pry
+      genre = split[1].match(/(?<=\[).+?(?=\])/).to_s
+      @@songs_info << [artist, song, genre]
     end
   end
 
-    # hash = {"Artist" =>
-    #           :genres =>
-    #             :folk => [songs]
-    #             :rap => [songs]
-    #        }
+  def self.artist_exists?(song_as_array)
+    Artist.all.select do |a|
+      a.name == song_as_array[0]
+    end
+  end
+
+  def self.assign_songs_info
+    @@songs_info.each do |song|
+      if artist_exists?(song).size == 0
+        artist = Artist.new.tap { |a| a.name = song[0] }
+      else
+        artist = artist_exists?(song).first
+      end
+    end
+  end
+
 
 end
 
 SongPal.split_song_info
-
-# Get all the file names from data directory
-
-# Split them up into artist, song name, and genre (hash?)
+SongPal.assign_songs_info
 
 # For every file, make the artist, then the genre, then the song?
